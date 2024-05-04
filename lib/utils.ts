db8645df7ca832a7a8f7d88724ffe5c1148fd278
Eric Broberg <eric.edulis@gmail.com>
2024-05-04 15:38:54 -0700

    finish installing shad cn

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// tailwind-merge is a utility that merges Tailwind CSS classes together.
