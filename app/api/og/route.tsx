3f37b4c92e8fce2745e2dec72c8ff2c78217627a
Eric Broberg <eric.edulis@gmail.com>
2024-05-24 18:59:12 -0700

    image and dynamic graph image

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { ImageResponse } from "next/og";
+import { ImageResponse } from "next/og"; // og means open graph - what appears when you share link on social media
 import { siteConfig } from "@/config/site";
 
 export const runtime = "edge";
@@ -8,6 +8,7 @@ const interBold = fetch(
   new URL("../../../assets/fonts/Inter-Bold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
+// this API is for getting an image for SEO/metadata
 export async function GET(req: NextRequest) {
   try {
     const fontBold = await interBold;
@@ -22,24 +23,12 @@ export async function GET(req: NextRequest) {
     const heading =
       title.length > 140 ? `${title.substring(0, 140)}...` : title;
 
+    // creating an image out of our blog title
     return new ImageResponse(
       (
         <div tw="flex relative flex-col p-12 w-full h-full items-start text-black bg-white">
           <div tw="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="M4 11a9 9 0 0 1 9 9" />
-              <path d="M4 4a16 16 0 0 1 16 16" />
-              <circle cx="5" cy="19" r="1" />
-            </svg>
-            <p tw="ml-2 font-bold text-2xl">JollyBlog</p>
+            <p tw="ml-2 font-bold text-2xl">🏝️ Eric's Blog</p>
           </div>
           <div tw="flex flex-col flex-1 py-10">
             <div tw="flex text-xl uppercase font-bold tracking-tight font-normal">
