42847f028c5cda7ba252589c3f2a6fac3d76c6e0
Eric Broberg <eric.edulis@gmail.com>
2024-05-28 14:22:46 -0700

    update website link

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
   name: "Eric Broberg",
-  url: "https://eric_tbd.dev", // TODO update this
+  url: "https://site-psi-two.vercel.app/", // TODO update this
   description: 'the web page of eric broberg',
   author: "Eric Broberg",
   links: {
     twitter: 'https://twitter.com/ebrodev',
     github: 'https://github.com/edulis8',
     linkedin: 'https://www.linkedin.com/in/ericbroberg/',
-    personalSite: "https://eric_tbd.dev",
+    personalSite: "https://site-psi-two.vercel.app/",
   },
 };
 
