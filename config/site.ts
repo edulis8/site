f60a200dea5ea22d4bf081e169c4ac7575ee1003
Eric Broberg <eric.edulis@gmail.com>
2024-05-28 14:07:53 -0700

    some configs

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
   name: "Eric Broberg",
-  url: "https://eric.dev", // TODO update this
+  url: "https://eric_tbd.dev", // TODO update this
   description: 'the web page of eric broberg',
   author: "Eric Broberg",
   links: {
     twitter: 'https://twitter.com/ebrodev',
     github: 'https://github.com/edulis8',
     linkedin: 'https://www.linkedin.com/in/ericbroberg/',
-    personalSite: "https://eric.dev",
+    personalSite: "https://eric_tbd.dev",
   },
 };
 
