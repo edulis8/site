53f178ea4c0cc8ec4365501b730a41b422b84df8
Eric Broberg <eric.edulis@gmail.com>
2024-05-28 16:12:58 -0700

    first blog post that is a real one.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,15 +40,8 @@ export default async function AboutPage() {
           </p>
         </div>
         <p className="text-muted-foreground text-lg py-4">
-          TODO: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-
-          TODO: resume.
+          👋 I'm Eric, software engineer. 
+          I'll write more about myself here later. 
         </p>
       </div>
     </div>
